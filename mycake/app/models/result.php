b699f24e4dc586f31584232bf9aab7546931001f
andreaurioli <andreaurioli@2047b526-045f-11df-8b0e-05a2803efa1e>
2010-01-21 16:57:31 +0000

    Model per la tabella Results

---
+++ mycake/app/models/result.php
@@ -0,0 +1,16 @@
+<?php
+class Result extends AppModel
+{
+	var $name = 'Result';
+	var $belongsTo = array(
+				 'Wordslist' =>
+                 array('className'    => 'Wordslist',
+                     'foreignKey'   => 'wordslist_id'
+                 ),
+				 'User' =>
+                 array('className'    => 'User',
+                     'foreignKey'   => 'user_id'
+                 )
+             );             
+}
+?>
