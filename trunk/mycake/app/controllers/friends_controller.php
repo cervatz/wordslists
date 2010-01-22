c9fcd91f11adc955a73c1cee8ad5c0e5bec0c40a
andreaurioli <andreaurioli@2047b526-045f-11df-8b0e-05a2803efa1e>
2010-01-22 17:15:33 +0000

    Conferma di amicizia

--- trunk/mycake/app/controllers/friends_controller.php
+++ trunk/mycake/app/controllers/friends_controller.php
@@ -39,15 +39,68 @@ class FriendsController extends SuperController
 	{
 		$this->log('FriendsController myfriends() - entering ...',LOG_DEBUG);
 		$this->pageTitle = 'My friends';
-
+		
 		$friends = $this->Friend->find('all',
 			array(
 				'conditions' => array('Friend.user_id1' => $this->Security->retrieveUserId()),
 				'order' => array('Friend.status DESC', 'User2.username')
+			));		
+
+		$requests = $this->Friend->find('all',
+			array(
+				'conditions' => array('Friend.user_id2' => $this->Security->retrieveUserId(),
+									  'Friend.status' => 0
+									 ),
+				'order' => array('User2.username')
 			));
 
 		$this->set('friends', $friends);
+		$this->log('RICHIESTE: '.$requests,LOG_DEBUG);
+		$this->set('requests', $requests);
 		
+	}	
+	
+	function confirm($id, $friend_id)
+	{
+		$this->log('FriendsController add() - entering ...',LOG_DEBUG);
+		
+		//$this->log('ID['.$id.']',LOG_DEBUG);
+		
+		if (!empty($id)) {
+		
+			// AU - Creo la nuova relazione
+			$this->data['Friend']['user_id1']=$id;
+			$this->data['Friend']['user_id2']=$this->Security->retrieveUserId();
+			$this->data['Friend']['status']=1;
+			
+			$this->log('Creo nuova relazione',LOG_DEBUG);
+			$this->log($this->data,LOG_DEBUG);
+			
+			if ($this->Friend->save($this->data)) {
+			
+				// AU - Aggiorno la vecchia richiesta
+				$this->data['Friend']['id']=$friend_id;
+				$this->data['Friend']['user_id1']=$this->Security->retrieveUserId();
+				$this->data['Friend']['user_id2']=$id;
+				$this->data['Friend']['status']=1;
+				
+				$this->log('Aggiorno la vecchia relazione',LOG_DEBUG);
+				$this->log($this->data,LOG_DEBUG);				
+			
+				if ($this->Friend->save($this->data)) {
+					$this->Session->setFlash(__('message_friends_created',true));												
+					$this->redirect(array('controller'=>'friends','action' => 'myfriends'));				
+				}			
+				else{
+					$this->Session->setFlash(__('message_friends_not_updated',true));
+				}
+								
+			}
+			else{
+				$this->Session->setFlash(__('message_friends_not_created',true));
+			}
+			
+		}
 	}	
 		
 }
