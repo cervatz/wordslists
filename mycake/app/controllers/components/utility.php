d66d60d7b9f4eaefcd190ee95c133726153ac1ac
andreaurioli <andreaurioli@2047b526-045f-11df-8b0e-05a2803efa1e>
2010-01-22 16:38:21 +0000

    Gestione amicizie

--- mycake/app/controllers/components/utility.php
+++ mycake/app/controllers/components/utility.php
@@ -3,7 +3,7 @@ class UtilityComponent extends Object {
 
 	var $uses = array('Wordslist');
 
-	var $components = array('Session');
+	var $components = array('Session', 'Security');
 
 	function getLanguages()
 	{
@@ -27,7 +27,7 @@ class UtilityComponent extends Object {
 		return $languages; 
 	}
 	
-	function getUsers()
+	function getUsers($id)
 	{
 
 		if (App::import('Model', 'User')) {			 
@@ -36,15 +36,54 @@ class UtilityComponent extends Object {
 
 		$users = $this->User->find
 			(
-				'list',  
+				'all',  
 				array
-					(
-                     'fields' => array('id', 'username', 'first_name', 'last_name'),  
+					('conditions' => array('User.id' => $id),  
                      'order' => 'username ASC',  
                      'recursive' => -1  
 				)
 			);
 			
+			
+		return $users; 
+	}	
+
+	function getFriends()
+	{
+
+		if (App::import('Model', 'Friend')) {			 
+			$this->Friend = new Friend();
+		}
+		
+		if (App::import('Model', 'User')) {			 
+			$this->User = new User();
+		}		
+		
+		$friends = $this->Friend->find('all',
+			array(
+					'conditions' => array('Friend.user_id1' => $this->Security->retrieveUserId())
+			));		
+	
+		//$this->log($friends,LOG_DEBUG);
+		
+		// TODO - AU - Questa non è il massimo come soluzione, ma al momento non sapevo come inserire
+		// TODO - AU - una condizione NOT IN nell'SQL.. ci ho anche provato ma non ne sono venuto fuori
+		// TODO - AU - Creo un array con gli user_id degli utenti già associati all'utente in sessione
+		// TODO - AU - e inserisco una clausola NOT nell'SQL più in basso
+		foreach ($friends as $myarray) {
+			$myfriends[] = $myarray['Friend']['user_id2'];
+		}	
+			
+		$users = $this->User->find
+			(
+				'list',  
+				array
+					('conditions' => array("User.id" => $myfriends),
+                     'fields' => array('id', 'username', 'first_name', 'last_name'),  
+                     'order' => 'username ASC',  
+                     'recursive' => -1  
+				)
+			);			
 			
 		return $users; 
 	}	
